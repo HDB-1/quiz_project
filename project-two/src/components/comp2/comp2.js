4a1661871dfd8a4242aac9af64bc25b3b55d9c1f
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-12-16 09:37:55 +0000

    template components

---
+++ project-two/src/components/comp2/comp2.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './comp2.css'
+
+export class comp2 extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default comp2

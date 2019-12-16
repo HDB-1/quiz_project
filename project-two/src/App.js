f381186a1ba31ebfdc80883591be91a3afe71c45
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2019-12-16 12:53:26 +0000

    added routing and initial routing tests

--- project-two/src/App.js
+++ project-two/src/App.js
@@ -2,15 +2,23 @@ import React from 'react';
 import './App.css';
 import Quiz from "./containers/Quiz/Quiz";
 import QuizSetup from "./components/QuizSetup/QuizSetup";
+import { BrowserRouter as Router, Switch, Link, Route } from 'react-router-dom';
 
 class App extends React.Component {
 
   render(){
   return (
-    <div>
-      <Quiz />
-      <QuizSetup />
-    </div>
+    <Router className="App">
+      
+      <Switch>
+        <Route exact path="/">
+          <QuizSetup />
+        </Route>
+        <Route path="/quiz">
+          <Quiz />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 }
--- project-two/tests/App.test.js
+++ project-two/tests/App.test.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import App from '../src/App';
-import { shallow } from 'enzyme';
+import { shallow , mount } from 'enzyme';
 import Quiz from '../src/containers/Quiz/Quiz'
 import QuizSetup from '../src/components/QuizSetup/QuizSetup'
+import { MemoryRouter } from 'react-router-dom';
 
-describe('App', () => {
+describe('Shallow App', () => {
   let wrapper;
   
   beforeEach(() => wrapper = shallow(<App />)); // this automatically tests for rendering without crashing.
@@ -15,7 +16,21 @@ describe('App', () => {
   it("Should render a <Quiz /> component", () => {
     expect(wrapper.containsMatchingElement(<Quiz />)).toEqual(true);
   })
+
   it("Should render a <QuizSetup /> component", () => {
+    expect(wrapper.containsMatchingElement(<QuizSetup />)).toEqual(true);
+  })
+})
+
+describe('Mounted App', () => {
+  it("Should render quiz component on route '/quiz'", () => {
+    let wrapper = mount(<MemoryRouter initialEntries={['/quiz']}><App /></MemoryRouter>);
+    expect(wrapper.containsMatchingElement(<Quiz />)).toEqual(true);
+    expect(wrapper.containsMatchingElement(<QuizSetup />)).toEqual(false);
+  })
+  it("Should render quizSetup component on route '/'", () => {
+    let wrapper = mount(<MemoryRouter initialEntries={['/']}><App /></MemoryRouter>);
+    expect(wrapper.containsMatchingElement(<Quiz />)).toEqual(false);
     expect(wrapper.containsMatchingElement(<QuizSetup />)).toEqual(true);
   })
 })

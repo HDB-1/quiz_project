7520a049105d676a727939c1dc0666c84991dfc1
Dominic Holm-Kander <dominic.holmkander@gmail.com>
2019-12-16 16:12:51 +0000

    Add dropdown for question category and the requisite tests

--- project-two/src/components/QuizSetup/QuizSetup.js
+++ project-two/src/components/QuizSetup/QuizSetup.js
@@ -4,11 +4,22 @@ class QuizSetup extends Component {
     render() {
         return (
             <div>
-                <select>
+                <select className="difficultyDropDown">
                     <option value="easy">Easy</option>
                     <option value="medium">Medium</option>
                     <option value="hard">Hard</option>
                 </select>
+                <select className="categoryDropDown">
+                    <option value="9">General Knowledge</option>
+                    <option value="21">Sports</option>
+                    <option value="17">Science & Nature</option>
+                    <option value="23">History</option>
+                    <option value="28">Vehicles</option>
+
+
+
+                </select>
+                <button onClick={[Function]}>This is a button</button>
             </div>
         )
     }
--- project-two/tests/QuizSetup.test.js
+++ project-two/tests/QuizSetup.test.js
@@ -10,8 +10,8 @@ describe('Shallow QuizSetup', () => {
   it("Should render correctly", () => {
     expect(wrapper).toMatchSnapshot();
   })
-  it("Should render a dropdown", () => {
-      expect(wrapper.find('select').length).toEqual(1);
+  it("Should render two dropdowns", () => {
+      expect(wrapper.find('select').length).toEqual(2);
   })
   it("Difficulty dropdown should contain 3 difficulty options as both text and value properties (<option />)", () => {
       expect(wrapper.find('select').at(0).find('option').length).toEqual(3);
@@ -23,4 +23,30 @@ describe('Shallow QuizSetup', () => {
       expect(wrapper.find('select').at(0).find('option').at(1).props()).toHaveProperty("value", "medium")
       expect(wrapper.find('select').at(0).find('option').at(2).props()).toHaveProperty("value", "hard")
   })
+  it("Category dropdown should contain 5 (for now) topic options in <option /> tags", () => {
+      // Test for VALUE of categories for API querying
+    expect(wrapper.find('select').at(1).find('option').length).toEqual(5);
+    expect(wrapper.find('select').at(1).find('option').at(0).props()).toHaveProperty("value", "9") // General Knowledge
+    expect(wrapper.find('select').at(1).find('option').at(1).props()).toHaveProperty("value", "21") // Sports
+    expect(wrapper.find('select').at(1).find('option').at(2).props()).toHaveProperty("value", "17") // Science and Nature
+    expect(wrapper.find('select').at(1).find('option').at(3).props()).toHaveProperty("value", "23") // History
+    expect(wrapper.find('select').at(1).find('option').at(4).props()).toHaveProperty("value", "28") // Vehicles
+
+    // Test for text displayed for each dropdown.
+    expect(wrapper.find('select').at(1).find('option').at(0).text()).toEqual("General Knowledge") // General Knowledge
+    expect(wrapper.find('select').at(1).find('option').at(1).text()).toEqual("Sports") // Sports
+    expect(wrapper.find('select').at(1).find('option').at(2).text()).toEqual("Science & Nature") // Science and Nature
+    expect(wrapper.find('select').at(1).find('option').at(3).text()).toEqual("History") // History
+    expect(wrapper.find('select').at(1).find('option').at(4).text()).toEqual("Vehicles") // Vehicles
+  })
+
+  it("Should render a button", () => {
+    expect(wrapper.find('button').length).toEqual(1);
+  })
+
+  it("Button should have an onclick function", () => {
+    expect(wrapper.find('button').at(0).props()).toHaveProperty("onClick", [Function])
+  })
+
 });
+

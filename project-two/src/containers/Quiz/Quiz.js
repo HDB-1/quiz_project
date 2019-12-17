e0b38c5684adef91bc4f575e809bb35b9b3f5f8e
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-12-17 10:28:46 +0000

    mock fetch test setup for expected quiz setup data

--- project-two/src/containers/Quiz/Quiz.js
+++ project-two/src/containers/Quiz/Quiz.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react'
 
+
+export function APIRequest(quizInfo) {
+    return fetch('https://opentdb.com/api.php?amount=5').then(res => res.json())
+}
 class Quiz extends Component {
 
-    
+    componentDidMount() {
+        console.log('quiz component mounted');
+        
+    }
+
+
     render() {
         return (
             <div>
-                
             </div>
         )
     }
--- project-two/tests/Quiz.test.js
+++ project-two/tests/Quiz.test.js
@@ -1,17 +1,35 @@
 import React from 'react';
 import { shallow , mount } from 'enzyme';
 import Quiz from '../src/containers/Quiz/Quiz';
+import {APIRequest} from '../src/containers/Quiz/Quiz';
 
 
 
-describe('Shallow NavBar', () => {
+
+describe('Shallow Quiz', () => {
   let wrapper;
   
   beforeEach(() => wrapper = shallow(<Quiz />)); // this automatically tests for rendering without crashing.
 
   it("Should render correctly", () => {
     expect(wrapper).toMatchSnapshot();
-  })
+  });
+});
 
-  
+describe('testing API', () => {
+  // beforeEach(() => {fetch.resetMocks()});
+  it('calls API and returns data to me', () => {
+    //Mock fetch response data
+    fetch.mockResponseOnce(JSON.stringify({ data: '12345' }))
+
+    //assert on the response
+    APIRequest('').then(res => {
+      expect(res.data).toEqual('12345')
+    })
+    //assert on the time called and arguments given to fetch
+    expect(fetch.mock.calls.length).toEqual(1)
+    console.log(fetch.mock.calls)
+    expect(fetch.mock.calls[0][0]).toEqual('https://opentdb.com/api.php?amount=5')
+
+  })
 })

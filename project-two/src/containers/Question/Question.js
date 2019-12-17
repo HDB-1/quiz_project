3df67eacfb4598d743c2de7a74d3e2c10f7efba5
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-12-17 14:48:53 +0000

    add render tests for Question children: Answers and Submit

--- project-two/src/containers/Question/Question.js
+++ project-two/src/containers/Question/Question.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
-
+import Answers from '../../components/Answers/Answers'
+import Submit from '../../components/Submit/Submit'
 export class Question extends Component {
     render() {
         return (
             <div>
-                
+                <Answers/>
+                <Submit/>
             </div>
         )
     }
 }
-
 export default Question

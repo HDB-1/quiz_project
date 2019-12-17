43b7bfb3ceee810ad862edc23cb7f130c245befb
Harry DB <57152441+HDB-1@users.noreply.github.com>
2019-12-17 14:30:16 +0000

    check info children render their information correctly

--- project-two/src/components/Info/Info.js
+++ project-two/src/components/Info/Info.js
@@ -2,9 +2,11 @@ import React from 'react'
 import Title from './Title/Title'
 import Progress from './Progress/Progress'
 import User from './User/User'
+import './Info.css'
+
 export default function Info(props) {
     return (
-        <div>
+        <div className='info'>
             <User currentUser={props.users}/>
             <Title title={props.title}/>
             <Progress question={{current:1, total:10}}/>

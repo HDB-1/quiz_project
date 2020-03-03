6a77322eeb5d98083c29ec71f4d39a8c8f765b54
Harry DB <57152441+HDB-1@users.noreply.github.com>
2020-03-03 12:03:11 +0000

    deploying to heroku

---
+++ project-two/build/precache-manifest.4b7e2dab6d8a5ea460a3515b8cacfbf6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe4af9f64337fecfcdfd2891eea8d0dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0ecdbf0f52d72b08a0c4",
+    "url": "/static/css/main.465cb8e9.chunk.css"
+  },
+  {
+    "revision": "77c8e04f689b7cd84c3c",
+    "url": "/static/js/2.851204b7.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/static/js/2.851204b7.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ecdbf0f52d72b08a0c4",
+    "url": "/static/js/main.51d95aec.chunk.js"
+  },
+  {
+    "revision": "7b83425522fccecae055",
+    "url": "/static/js/runtime-main.abddd0e0.js"
+  }
+]);
